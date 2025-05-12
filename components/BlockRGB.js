09eca0631ba2ce418286d753886477df01c0c7f5
YJ Soon <yjsoon@tinkertanker.com>
2025-05-12 16:30:04 +0800

    Aligned blocks at top, now full width

--- components/BlockRGB.js
+++ components/BlockRGB.js
@@ -4,7 +4,7 @@ const BlockRGB = ({ red, green, blue }) => {
   return (
     <View
       style={{
-        width: 100,
+        width: "100%",
         height: 100,
         backgroundColor: `rgb(${red}, ${green}, ${blue})`
       }}
