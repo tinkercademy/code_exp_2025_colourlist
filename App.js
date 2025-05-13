54b4954376506e0d9ab4c77a435145d7adb29949
YJ Soon <yjsoon@tinkertanker.com>
2025-05-13 11:20:47 +0800

    Style 'Add colour' button with blue background

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, FlatList, View, Button } from "react-native";
+import { StyleSheet, FlatList, View, Pressable, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import BlockRGB from "./components/BlockRGB";
 import { useState } from "react";
 
 function HomeScreen() {
-  const [colours, setColours] = useState([]);
+  const [colours, setColours] = useState([
+    // Initial values with 3 colours
+    { red: 255, green: 0, blue: 0, key: "1" },
+    { red: 0, green: 255, blue: 0, key: "2" },
+    { red: 0, green: 0, blue: 255, key: "3" }
+  ]);
 
   function renderItem({ item }) {
     return <BlockRGB red={item.red} green={item.green} blue={item.blue} />;
@@ -18,7 +23,7 @@ function HomeScreen() {
         red: Math.floor(Math.random() * 256),
         green: Math.floor(Math.random() * 256),
         blue: Math.floor(Math.random() * 256),
-        id: colours.length
+        key: Math.random().toString()
       },
       ...colours // Take whatever is there, and "spread" it into this array
     ]);
@@ -26,7 +31,9 @@ function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <Button onPress={addColour} title="Add colour" />
+      <Pressable style={styles.addButton} onPress={addColour}>
+        <Text style={styles.addButtonText}>Add colour</Text>
+      </Pressable>
       <FlatList
         style={{ width: "100%" }}
         data={colours}
@@ -55,5 +62,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "top"
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    marginVertical: 10
+  },
+  addButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center'
   }
 });
